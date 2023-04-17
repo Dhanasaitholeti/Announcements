c8b203b315995dfc0c03f88fbead218f39c48fca
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-04-17 07:55:37 +0530

    removed frontend files

--- Announcements-client/src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-const Loading = () => {
-    return ( 
-        <h1>This is Loading page</h1>
-     );
-}
- 
-export default Loading;
--- Announcements-client/src/components/NoAuth.jsx
+++
@@ -1,7 +0,0 @@
-const NoAuth = () => {
-    return ( 
-        <h1>You are not authenticated</h1>
-     );
-}
- 
-export default NoAuth;
