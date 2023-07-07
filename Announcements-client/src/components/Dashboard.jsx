72e07bc59ff7e01df991b9e742cc894480fdc48d
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-07-07 21:30:06 +0530

    changes:added grid in Dashboard page using chakra ui and added styles

--- Announcements-client/src/components/Dashboard.jsx
+++ Announcements-client/src/components/Dashboard.jsx
@@ -1,29 +1,40 @@
-import "../styles/Dashboard.css";
+import { Box, Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
+
+import { Route, Routes } from "react-router-dom";
 import checkToken from "./checks/checkToken";
 import Announcement from "./Announcements";
 import UserDetails from "./UserDetails";
-import { Route, Routes } from "react-router-dom";
 import AnnouncementForm from "./AnnouncementForm";
-import NotFound from "./NotFound";
 import ShowAnnouncement from "./ShowAnnoucement";
+import NotFound from "./NotFound";
 
 const Dashboard = () => {
+  const boxstyles = {
+    boxShadow: "dark-lg",
+    borderRadius: 5,
+    p: 5,
+  };
+
   checkToken();
 
   return (
-    <div className="Main-Area">
-      <div className="Announcement-part">
-        <Routes>
-          <Route path="/" element={<Announcement />} />
-          <Route path="/new-announcement" element={<AnnouncementForm />} />
-          <Route path="/announcement/:id" element={<ShowAnnouncement />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </div>
-      <div className="userdetails-part">
-        <UserDetails />
-      </div>
-    </div>
+    <Grid templateColumns="repeat(6,1fr)" gap={5} p={3}>
+      <GridItem colSpan={4}>
+        <Box {...boxstyles}>
+          <Routes>
+            <Route path="/" element={<Announcement />} />
+            <Route path="/new-announcement" element={<AnnouncementForm />} />
+            <Route path="/announcement/:id" element={<ShowAnnouncement />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </Box>
+      </GridItem>
+      <GridItem colStart={5} colSpan={2}>
+        <Box {...boxstyles}>
+          <UserDetails />
+        </Box>
+      </GridItem>
+    </Grid>
   );
 };
 
